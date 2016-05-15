4646e1fd44cd4845c8110392be18efb2c16a2d9b
Aaron Toys <aarontoys@gmail.com>
2016-05-15 16:07:14 -0600

    add register functionality to front-end

--- src/client/app/register/register.directive.js
+++ src/client/app/register/register.directive.js
@@ -4,19 +4,38 @@ angular
   .module('myApp')
   .directive('appRegister', register);
 
-function register () {
-  return {
-    restrict: 'EA',
-    templateUrl: 'app/register/register.view.html',
-    controller: registerCtrl,
-    controllerAs: 'vm',
+  function register () {
+    return {
+      restrict: 'EA',
+      templateUrl: 'app/register/register.view.html',
+      controller: registerCtrl,
+      controllerAs: 'vm',
+    }
   }
-}
 
-function registerCtrl () {
-  var vm = this;
+  registerCtrl.$inject = ['registerDataService'];
 
-  vm.showReg = true;
-  console.log('showReg', vm.showReg);
-}
+  function registerCtrl (registerDataService) {
+    var vm = this;
+
+    vm.showReg = true;
+    console.log('showReg', vm.showReg);
+
+    vm.addNewUser = function () {
+      console.log('register clicked');
+      addUser(vm);
+    }
+
+    function addUser (user) {
+      console.log('about to add: ', user);
+      registerDataService.addUser(user)
+      .then(function(result) {
+        console.log('user added!')
+      })
+      .catch(function(err) {
+        console.log('user not added!');
+      });
+    }
+  }
+
 })();
---
+++ src/client/app/register/register.service.js
@@ -0,0 +1,29 @@
+(function () {
+
+  angular
+    .module('myApp')
+    .service('registerDataService', registerDataService);
+
+    registerDataService.$inject = ['$http'];
+
+    function registerDataService ($http) {
+
+      return {
+        addUser: function (user) {
+          console.log('registerDataService: user: ', user);
+          return $http.post('/register', {
+            email: user.email,
+            pword: user.pword
+          })
+          .then(function(res) {
+            console.log('line 19: ', res);
+            return res;
+          })
+          .catch(function(err) {
+            console.log('err line23: ', err);
+            return err;
+          });
+        }
+      }
+    }
+})();
